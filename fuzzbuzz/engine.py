1a0b69e348035ca94c8aede075964d1b29f68858
Tim Henderson <tim.tadh@gmail.com>
2012-01-05 12:47:16 -0500

    stubs for engine, actions, and conditions
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

---
+++ fuzzbuzz/engine.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Tim Henderson
+#Email: tim.tadh@hackthology.com
+#For licensing see the LICENSE file in the top level directory.
+
+## Should contain the implementation of the main fuzzing algorithm.
---
+++ fuzzbuzz/models/condition.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Tim Henderson
+#Email: tim.tadh@hackthology.com
+#For licensing see the LICENSE file in the top level directory.
+
+class Condition(object): pass
