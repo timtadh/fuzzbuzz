4e18c8fdc67228c3c4fcefd91e7a118f348029c5
Tim Henderson <tim.tadh@gmail.com>
2012-01-13 14:41:41 -0500

    fitfully worked on the model of the grammar.
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- fuzzbuzz/models/grammar.py
+++ fuzzbuzz/models/grammar.py
@@ -8,11 +8,25 @@ class Grammar(object):
 
     def __init__(self, rules):
         self.rules = rules
+        self.symbols = dict()
+        for rule in rules:
+            nt = self.symbols.get(rule.name, NonTerminal(rule.name))
+            nt.addrule(rule)
+            self.symbols[rule.name] = nt
+        print self.symbols
 
     def __str__(self):
         return super(Grammar, self).__str__()[:-1] + ' ' + str(self.rules) + '>'
 
 class NonTerminal(object):
 
-    def __init__(self, rules):
-        pass
+    def __init__(self, name):
+        self.rules = list()
+        self.name = name
+
+    def addrule(self, rule):
+        self.rules.append(rule)
+
+    def __repr__(self): return str(self)
+    def __str__(self):
+        return super(NonTerminal, self).__repr__()[:-1] + ' ' + str(self.rules) + '>'
