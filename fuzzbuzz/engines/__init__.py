cf6166be5cea62aa5b38ea338431f2ac86020994
Rafael Lopez <rafael@case.edu>
2012-02-22 01:49:24 -0500

    Initial (and recursive) implementation of the stats engine. Currently very silly and dumb
    Signed-off-by: Rafael Lopez <rafael@case.edu>

--- fuzzbuzz/engines/__init__.py
+++ fuzzbuzz/engines/__init__.py
@@ -7,8 +7,8 @@
 ## We have to import all of our fuzzer modules otherwise they don't get
 ## registered. There is no way to work around this because the initialization
 ## code doesn't get run unless it is imported.
-import attribute, stub
-del stub, attribute
+import attribute, stub, cfgstats
+del stub, attribute, cfgstats
 
 ## Make registration available at the package level.
 from reg import registration
---
+++ fuzzbuzz/engines/cfgstats.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#Author: Rafael Lopez
+#Email: rafael.lopez.u@gmail.com
+#For licensing see the LICENSE file in the top level directory.
+from random import seed, choice, randint, random
+
+from reg import registration
+from fuzzbuzz.models.symbols import Terminal, NonTerminal
+
+@registration.register(
+  {'stat_tables':'tables'},
+  'A fuzzer to produce output based on statistics provided at input')
+def cfgstats(rlexer, grammar, stat_tables=None):
+    # grammar.start --> start symbol it is a fuzzbuzz.models.symbols.NonTerminal
+    # nonterm.rules --> a list of the possible choices.
+    # rule -> list of Terminal and NonTerminal symbols
+    # getnextrule(nonterm): random.choice(nonterm.rules)
+    
+    output = list()
+    def fuzz(nonterm):
+      rule = choice(nonterm.rules)
+      
+      for sym, cnt in rule.pattern:
+          if sym.__class__ is NonTerminal:
+              fuzz(sym)
+          if sym.__class__ is Terminal:
+            output.append(sym.name)
+    
+    fuzz(grammar.start)
+    return output
