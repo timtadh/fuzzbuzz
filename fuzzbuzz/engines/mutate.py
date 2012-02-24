6ee2584af85b8920f8da7c7ae39831818091a892
John Gunderman <johngunderman@gmail.com>
2012-02-24 15:34:56 -0500

    add generate_parser stub

--- fuzzbuzz/engines/mutate.py
+++ fuzzbuzz/engines/mutate.py
@@ -33,9 +33,22 @@ def mutation_fuzzer(rlexer, grammar, example_list=None):
     def generate_ast(example):
         """Generates and returns an AST for the provided `example`
         AST generated according to the provided grammar
+        On completion, returns an AST object.
 
         Arguments:
         - `example`: the text to be parsed into an AST
         """
         pass
+
+    def generate_parser():
+        """Generate a parser for our `example_list` based on the grammar in
+        `grammar`. This is probably going to be very hacky.
+        On completion this function returns a `parser` object which can be
+        used to generate ASTs for our examples.
+        """
+        pass
+
+    parser = generate_parser(grammar.start)
+    generate_ast(example) # should be generate_ast(example, parser). I'll fix this.
+    return output
 
